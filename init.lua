af84d0d838c046a5e1db2ef05e41d84a854c8076
Gabriel Haugbøl <gabriel.haugbol@gmail.com>
2024-11-10 11:18:24 +0100

    Add tailwind lsp

--- init.lua
+++ init.lua
@@ -644,6 +644,26 @@ require('lazy').setup({
         },
         volar = {},
         omnisharp = {},
+        tailwindcss = {
+          classAttributes = { 'class', 'className', 'class:list', 'classList', 'ngClass' },
+          includeLanguages = {
+            eelixir = 'html-eex',
+            eruby = 'erb',
+            htmlangular = 'html',
+            templ = 'html',
+          },
+          lint = {
+            cssConflict = 'warning',
+            invalidApply = 'error',
+            invalidConfigPath = 'error',
+            invalidScreen = 'error',
+            invalidTailwindDirective = 'error',
+            invalidVariant = 'error',
+            recommendedVariantOrder = 'warning',
+          },
+          validate = true,
+        },
+
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
